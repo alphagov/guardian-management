cc0029130fef2f4dda0b00b5b9ab832c7dfcadc4
Matthew Wall <mat.x.wall@gmail.com>
2011-12-28 15:03:30 +0000

    Allowed for stats reset

--- management/src/main/scala/com/gu/management/metrics.scala
+++ management/src/main/scala/com/gu/management/metrics.scala
@@ -1,47 +1,34 @@
 package com.gu.management
 
 import java.util.concurrent.atomic.AtomicLong
-import java.util.concurrent.Callable
 import java.util.Date
+import java.util.concurrent.Callable
 
-abstract class Metric()  {
+abstract class Metric() extends Loggable {
   def asJson: StatusMetric
+
+  def reset() {
+  }
 }
 
 object TimingMetric {
-  val empty = new TimingMetric("application", "Empty","Empty","Empty")
-}
-
-case class GaugeMetric(group: String, name: String, title: String, description: String, master: String = "none") extends Metric {
-  private val _count = new AtomicLong()
-
-  def recordCount(count: Int) {
-     _count.addAndGet(count)
-  }
-
-  def count = _count.get
-
- def asJson = StatusMetric(
-    group = group,
-    master = master,
-    name = name,
-    `type` = "gauge",
-    title = title,
-    description = description,
-    value = Some(count.toString)
-  )
+  val empty = new TimingMetric("application", "Empty", "Empty", "Empty")
 }
 
 case class CountMetric(group: String, name: String, title: String, description: String, master: String = "none") extends Metric {
   private val _count = new AtomicLong()
 
   def recordCount(count: Int) {
-     _count.addAndGet(count)
+    _count.addAndGet(count)
   }
 
   def count = _count.get
 
- def asJson = StatusMetric(
+  override def reset() {
+    _count.set(0)
+  }
+
+  def asJson = StatusMetric(
     group = group,
     master = master,
     name = name,
@@ -52,7 +39,7 @@ case class CountMetric(group: String, name: String, title: String, description: 
   )
 }
 
-case class TimingMetric(group: String, name: String, title: String, description: String, master: String = "none" ) extends Metric() {
+case class TimingMetric(group: String, name: String, title: String, description: String, master: String = "none") extends Metric() {
 
   private val _totalTimeInMillis = new AtomicLong()
   private val _count = new AtomicLong()
@@ -60,6 +47,11 @@ case class TimingMetric(group: String, name: String, title: String, description:
   def recordTimeSpent(durationInMillis: Long) {
     _totalTimeInMillis.addAndGet(durationInMillis)
     _count.incrementAndGet
+  }
+
+  override def reset() {
+    _count.set(0)
+    _totalTimeInMillis.set(0)
   }
 
   def asJson = StatusMetric(
@@ -74,16 +66,9 @@ case class TimingMetric(group: String, name: String, title: String, description:
   )
 
   def totalTimeInMillis = _totalTimeInMillis.get
+
   def count = _count.get
 
-  // to use this class, you can write your own wrappers
-  // and call recordTimeSpent, or you may use this one
-  // if you want.
-  // val t = TimingMetric("example")
-  // ...
-  // t measure {
-  //   code here
-  // }
   def measure[T](block: => T) = {
     val s = new StopWatch
     val result = block
@@ -92,33 +77,35 @@ case class TimingMetric(group: String, name: String, title: String, description:
   }
 
   // for java developers, these are easier to call
-  def call[T](c: Callable[T]) = measure { c.call }
-  def run(r: Runnable) = measure { r.run() }
+  def call[T](c: Callable[T]) = measure {
+    c.call
+  }
+
+  def run(r: Runnable) = measure {
+    r.run()
+  }
 }
 
-case class StatusMetric(
-  group: String = "application",
-  master: String = "none",
-  // name should be brief and underscored not camel case
-  name: String,
-  `type`: String,
-  // a short (<40 chars) title for this metric
-  title: String,
-  // an as-long-as-you-like description of what this metric means
-  // (used, e.g. on mouse over)
-  description: String,
-  // NB: these are deliberately strings - some json parsers have issues
-  // with big numbers, see https://dev.twitter.com/docs/twitter-ids-json-and-snowflake
-  value: Option[String] = None,
-  count: Option[String] = None,
-  totalTime: Option[String] = None,
-  units: Option[String] = None)
+case class StatusMetric(group: String = "application",
+                        master: String = "none",
+                        // name should be brief and underscored not camel case
+                        name: String,
+                        `type`: String,
+                        // a short (<40 chars) title for this metric
+                        title: String,
+                        // an as-long-as-you-like description of what this metric means
+                        // (used, e.g. on mouse over)
+                        description: String,
+                        // NB: these are deliberately strings - some json parsers have issues
+                        // with big numbers, see https://dev.twitter.com/docs/twitter-ids-json-and-snowflake
+                        value: Option[String] = None,
+                        count: Option[String] = None,
+                        totalTime: Option[String] = None,
+                        units: Option[String] = None)
 
-case class StatusResponseJson(
- application: String,
- time: Long = new Date().getTime,
- metrics: Seq[StatusMetric] = Nil
-)
+case class StatusResponseJson(application: String,
+                              time: Long = new Date().getTime,
+                              metrics: Seq[StatusMetric] = Nil)
 
 
 
